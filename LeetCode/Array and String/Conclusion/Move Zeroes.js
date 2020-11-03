0ab8f5df623ffa99e74bde5d1d1b53b2ce2ad2d9
yuriyfomin17 <fominyura2012@gmail.com>
2020-11-03 17:13:19 +0200

    updated

---
+++ LeetCode/Array and String/Conclusion/Move Zeroes.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ *
+ */
+// Time complexity is O(N) since we iterate through every element at once
+// SPace complexity is O(1) since we are not utilizing eny extra space
+const moveZeroes = function (nums) {
+    let count = nums.indexOf(0)
+    debugger
+    if (count !== -1) {
+        for (let i = count; i < nums.length; i++) {
+            if (nums[i] !== 0) {
+                let previous = nums[count]
+                nums[count] = nums[i]
+                nums[i] = previous
+                count++
+            }
+        }
+
+    }
+
+    return nums
+};
+
+console.log(moveZeroes([0, 1, 0, 3, 12]))
