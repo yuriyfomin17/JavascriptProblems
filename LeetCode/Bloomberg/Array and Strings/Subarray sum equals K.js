4a88558dc3c89831b4f8f3ee06590420793c4513
yuriyfomin17 <fominyura2012@gmail.com>
2020-11-05 13:50:49 +0200

    updated

--- LeetCode/Bloomberg/Array and Strings/Subarray sum equals K.js
+++ LeetCode/Bloomberg/Array and Strings/Subarray sum equals K.js
@@ -5,7 +5,7 @@
  */
 // Time complexity is O(N^2) since we iterate two times over the loop to find sum
 // Space Complexity is O(1) since we are not utilizing any extra space to store results
-const subarraySum = function (nums, k) {
+const subarraySumH = function (nums, k) {
     let count = 0
     let sum
     debugger
@@ -20,12 +20,12 @@ const subarraySum = function (nums, k) {
     }
     return count
 };
-const subarraySumH = function (nums, k) {
-    debugger
+const subarraySum = function (nums, k) {
     let map = new Map();
     let sum = 0;
     let count = 0;
     map.set(0, 1);
+    debugger
     for (let i = 0; i < nums.length; i++) {
         sum += nums[i];
         if (map.has(sum - k)) {
@@ -35,5 +35,8 @@ const subarraySumH = function (nums, k) {
     }
     return count;
 }
+subarraySum([1, 1, 1], 2)
 
-subarraySumH([1, 2, 3], 2)
+
+
+
