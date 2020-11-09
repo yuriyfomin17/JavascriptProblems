a71f3bf770173ab59f6df27fbf7747b412c54643
yuriyfomin17 <fominyura2012@gmail.com>
2020-11-09 16:11:19 +0200

    updated

---
+++ LeetCode/Bloomberg/Sorting and Searching/Median of Two Sorted Arrays.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+const findMedianSortedArrays = function (nums1, nums2) {
+    debugger
+    nums1.push(...nums2)
+    nums1.sort(function (a, b) {
+        if (a > b) {
+            return 1
+        } else {
+            return -1
+        }
+    })
+    let medIndex = nums1.length % 2 === 1 ? Math.trunc(nums1.length / 2) : [nums1.length / 2 - 1, nums1.length / 2]
+    if(Array.isArray(medIndex)){
+        return (nums1[medIndex[0]] + nums1[medIndex[1]])/2
+    }else{
+        return nums1[medIndex]
+    }
+};
+console.log(findMedianSortedArrays([1,2],[3,4]))
---
+++ LeetCode/Bloomberg/Sorting and Searching/Search in Rotated Array.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+//Time complexity is O(N)
+// Space complexity is O(1)
+const search = function(nums, target) {
+    return nums.indexOf(target)
+};
