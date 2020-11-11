6ace6b6523ac439a7eb68ff3f3a6be73bf449dde
yuriyfomin17 <fominyura2012@gmail.com>
2020-11-11 17:46:25 +0200

    updated

--- LeetCode/Ph questions/Ph/Binary Tree Vertical Order Traversal.js
+++ LeetCode/Ph questions/Ph/Binary Tree Vertical Order Traversal.js
@@ -8,6 +8,11 @@ function TreeNode(val, left, right) {
  * @param {TreeNode} root
  * @return {number[][]}
  */
+// Time Complexity is O(Nlog(N)) where N is the number of nodes in the tree
+// BFS runs O(N) times Then sorting the dictionary by the key can result in O(Nlog(N)) time complexity
+    // in the worst case scenario where the binary treee is extremally unbalanced . Like each node has only left child
+
+// Space Complexity is O(N) where N is the number of nodes in the tree
 const verticalOrder = function (root) {
     if (!root) {
         return []
@@ -43,3 +48,43 @@ const verticalOrder = function (root) {
     result.map(el => result2.push(el[1]))
     return result2
 };
+// Time Complexity is O(N)
+var verticalOrder = function(root) {
+    if (!root) return [];
+
+    root.offset = 0;
+    const queue = [root];
+
+    const res = [];
+    let rootLevel = 0;
+
+    while (queue.length) {
+        let node = queue.shift();
+
+        if (res[rootLevel + node.offset]) {
+            res[rootLevel + node.offset].push(node.val);
+        } else if (node.offset < 0) {
+            res.unshift([node.val]);
+            rootLevel++;
+        } else {
+            res.push([node.val]);
+        }
+
+        if (node.left) {
+            node.left.offset = node.offset - 1;
+            queue.push(node.left);
+        }
+        if (node.right) {
+            node.right.offset = node.offset + 1;
+            queue.push(node.right);
+        }
+    }
+
+    return res;
+};
+
+// Track the vertical level of the root node.
+//     Store the vertical level offset from the root node inside each queued node.
+//     If the vertical level corresponding to a visiting node doesn't exist on the left,
+//     we shift our results to the right and increment the root vertical level;
+//     for the right side we simply push on a new level.
