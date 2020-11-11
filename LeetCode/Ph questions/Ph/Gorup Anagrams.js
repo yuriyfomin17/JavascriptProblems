defb7f999f5d7a454f25623fd5d2126b04465194
yuriyfomin17 <fominyura2012@gmail.com>
2020-11-11 16:38:50 +0200

    updated

--- LeetCode/Ph questions/Ph/Gorup Anagrams.js
+++ LeetCode/Ph questions/Ph/Gorup Anagrams.js
@@ -2,9 +2,23 @@
  * @param {string[]} strs
  * @return {string[][]}
  */
+// Time Complexity: O(n*k) where n is the size of input array and k is the maximum length of string in input array
+// Space Complexity: O(n)
+var groupAnagrams1 = function(strs) {
+    let res = {};
+    debugger
+    for (let str of strs) {
+        let count = new Array(26).fill(0);
+        for (let char of str) count[char.charCodeAt()-97]++;
+        let key = count.join("#");
+        res[key] ? res[key].push(str) : res[key] = [str];
+    }
+    return Object.values(res);
+};
 
-
-const groupAnagrams1 = strs => {
+// TIme complexity is O(N*logK) where k is the length of the string and Nis the number of strings in array
+// Space Complexity is O(N)  where N is the number of strings
+const groupAnagrams2 = strs => {
     const map = {};
     for (let str of strs) {
         const sorted = str.split('').sort().join('');
@@ -12,47 +26,47 @@ const groupAnagrams1 = strs => {
     }
     return Object.values(map);
 };
-const groupAnagrams = function (strs) {
-    const result = []
-    let currResult
-    strs.map((firstString, index1) => {
-        strs.map((secondString, index2) => {
-            if (index2 !== 0) {
-                strs.splice(index1,1)
-                if (secondString.length === firstString.length) {
-                    currResult = [firstString]
-                    const dict = {}
-                    for (let i = 0; i < secondString.length; i++) {
-                        if (!dict[firstString[i]]) {
-                            dict[firstString[i]] = 1
-                        } else {
-                            dict[firstString[i]] = dict[firstString[i]] + 1
-                        }
-                        if (!dict[secondString[i]]) {
-                            dict[secondString[i]] = -1
-                        } else {
-                            dict[secondString[i]] = dict[secondString[i]] - 1
-                        }
-                    }
-                    let index = true
-                    for (let [key, value] of Object.entries(dict)) {
-                        if (value !== 0) {
-                            index = false
-                            break
-                        }
-                    }
-                    if (index) {
-                        strs.splice(index2,1)
-                        currResult.push(secondString)
-                    }
-
-                }
-            }
-        })
-        result.push(currResult)
-    })
-    return result
-};
+// const groupAnagrams3 = function (strs) {
+//     const result = []
+//     let currResult
+//     strs.map((firstString, index1) => {
+//         strs.map((secondString, index2) => {
+//             if (index2 !== 0) {
+//                 strs.splice(index1,1)
+//                 if (secondString.length === firstString.length) {
+//                     currResult = [firstString]
+//                     const dict = {}
+//                     for (let i = 0; i < secondString.length; i++) {
+//                         if (!dict[firstString[i]]) {
+//                             dict[firstString[i]] = 1
+//                         } else {
+//                             dict[firstString[i]] = dict[firstString[i]] + 1
+//                         }
+//                         if (!dict[secondString[i]]) {
+//                             dict[secondString[i]] = -1
+//                         } else {
+//                             dict[secondString[i]] = dict[secondString[i]] - 1
+//                         }
+//                     }
+//                     let index = true
+//                     for (let [key, value] of Object.entries(dict)) {
+//                         if (value !== 0) {
+//                             index = false
+//                             break
+//                         }
+//                     }
+//                     if (index) {
+//                         strs.splice(index2,1)
+//                         currResult.push(secondString)
+//                     }
+//
+//                 }
+//             }
+//         })
+//         result.push(currResult)
+//     })
+//     return result
+// };
 console.log(
-    groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"])
+    groupAnagrams1(["eat", "tea", "tan", "ate", "nat", "bat"])
 )
