59ca9452788a934c15527430c9d25f00fa84291e
yuriyfomin17 <fominyura2012@gmail.com>
2020-11-25 15:56:45 +0200

    updated

--- LeetCode/Ph questions/Ph/Design Browser History.js
+++ LeetCode/Ph questions/Ph/Design Browser History.js
@@ -1,3 +1,7 @@
+
+// Time Complexity is O(1)
+// Space Complexity is O(N) where N is the current number of links
+
 /**
  * @param {string} homepage
  */
@@ -54,18 +58,3 @@ BrowserHistory.prototype.forward = function (steps) {
  * var param_2 = obj.back(steps)
  * var param_3 = obj.forward(steps)
  */
-debugger
-const browserHistory = new BrowserHistory("leetcode.com");
-browserHistory.visit("google.com");       // You are in "leetcode.com". Visit "google.com"
-browserHistory.visit("facebook.com");     // You are in "google.com". Visit "facebook.com"
-browserHistory.visit("youtube.com");      // You are in "facebook.com". Visit "youtube.com"
-browserHistory.back(1);                   // You are in "youtube.com", move back to "facebook.com" return "facebook.com"
-browserHistory.back(1);                   // You are in "facebook.com", move back to "google.com" return "google.com"
-browserHistory.forward(1);                // You are in "google.com", move forward to "facebook.com" return "facebook.com"
-browserHistory.visit("linkedin.com");     // You are in "facebook.com". Visit "linkedin.com"
-browserHistory.forward(2);                // You are in "linkedin.com", you cannot move forward any steps.
-browserHistory.back(2);                   // You are in "linkedin.com", move back two steps to "facebook.com" then to "google.com". return "google.com"
-browserHistory.back(7);                   // You are in "google.com", you can move back only one step to "leetcode.com". return "leetcode.com"
-
-
-
