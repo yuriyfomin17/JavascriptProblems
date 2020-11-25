14534f1e99f554acdc742ac4ddc04a4883a58c41
yuriyfomin17 <fominyura2012@gmail.com>
2020-11-25 13:05:58 +0200

    updated

--- LeetCode/Ph questions/Ph/Intersection of Two Arrays I.js
+++ LeetCode/Ph questions/Ph/Intersection of Two Arrays I.js
@@ -1,11 +1,3 @@
-/**
- * @param {number[]} nums1
- * @param {number[]} nums2
- * @return {number[]}
- */
-
-// Time Complexity is O(n1 + n2) where n1 is the length of nums1 and n2 is the length of nums2
-// Space Complexity is O(N) where N is the size of dictionary
 const intersection = function (nums1, nums2) {
     const dict = {}
 
@@ -25,9 +17,3 @@ const intersection = function (nums1, nums2) {
     })
     return result
 };
-console.log(
-    intersection([4,9,5], [9,4,9,8,4])
-
-)
-
-
