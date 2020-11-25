7ac5aecd6ef72d3a5251a840aaa5c01160baca3c
yuriyfomin17 <fominyura2012@gmail.com>
2020-11-25 15:34:28 +0200

    updated

--- LeetCode/Ph questions/Ph/Design a LeaderBoard.js
+++ LeetCode/Ph questions/Ph/Design a LeaderBoard.js
@@ -4,6 +4,8 @@ function Player(id, score) {
 
 }
 
+// Time Complexity is O(1) for adding and reseting while for sorting is O(Nlog(N))
+// Space Complexity is O(2N) since we store N players in dict and N players in arr
 const Leaderboard = function () {
     this.dict = {}
     this.arr = []
@@ -14,6 +16,7 @@ const Leaderboard = function () {
  * @param {number} score
  * @return {void}
  */
+
 Leaderboard.prototype.addScore = function (playerId, score) {
     if (!this.dict[playerId]) {
         const node = new Player(playerId, score)
@@ -58,19 +61,3 @@ Leaderboard.prototype.reset = function (playerId) {
     }
 
 };
-
-debugger
-const leaderboard = new Leaderboard();
-leaderboard.addScore(1, 73);   // leaderboard = [[1,73]];
-leaderboard.addScore(2, 56);   // leaderboard = [[1,73],[2,56]];
-leaderboard.addScore(3, 39);   // leaderboard = [[1,73],[2,56],[3,39]];
-leaderboard.addScore(4, 51);   // leaderboard = [[1,73],[2,56],[3,39],[4,51]];
-leaderboard.addScore(5, 4);    // leaderboard = [[1,73],[2,56],[3,39],[4,51],[5,4]];
-leaderboard.top(1);           // returns 73;
-leaderboard.reset(1);         // leaderboard = [[2,56],[3,39],[4,51],[5,4]];
-leaderboard.reset(2);         // leaderboard = [[3,39],[4,51],[5,4]];
-leaderboard.addScore(2, 51);   // leaderboard = [[2,51],[3,39],[4,51],[5,4]];
-leaderboard.top(3);           // returns 141 = 51 + 51 + 39;
-
-
-
