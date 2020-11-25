a8c439743e45ba7fd93edc2fcdceed5837d60377
yuriyfomin17 <fominyura2012@gmail.com>
2020-11-25 10:46:26 +0200

    updated

--- LeetCode/Ph questions/Ph/Inser Delete GetRandom O(1).js
+++ LeetCode/Ph questions/Ph/Inser Delete GetRandom O(1).js
@@ -1,6 +1,55 @@
 /**
  * Initialize your data structure here.
  */
+const RandomizedSet = function () {
+    this.dict = {}
+    this.nums = []
+};
+
+/**
+ * Inserts a value to the set. Returns true if the set did not already contain the specified element.
+ * @param {number} val
+ * @return {boolean}
+ */
+RandomizedSet.prototype.insert = function (val) {
+    if (!this.dict[val]) {
+        this.dict[val] = this.nums.length
+        this.nums.push(val)
+        return true
+    }
+    return false
+};
+
+/**
+ * Removes a value from the set. Returns true if the set contained the specified element.
+ * @param {number} val
+ * @return {boolean}
+ */
+RandomizedSet.prototype.remove = function (val) {
+    if (this.dict[val]) {
+        this.nums.splice()
+        return true
+    }
+    return false
+};
+
+/**
+ * Get a random element from the set.
+ * @return {number}
+ */
+RandomizedSet.prototype.getRandom = function () {
+    const arr = Object.keys(this.dict)
+    const randomIndex = Math.floor(Math.random() * arr.length)
+    return arr[randomIndex]
+
+};
+
+
+
+
+/**
+ * Initialize your data structure here.
+ */
 var RandomizedSet = function () {
     this.set = {}
 };
