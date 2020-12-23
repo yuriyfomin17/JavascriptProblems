4d586dc97abb8f5be60fe733834ceee2f0ca29bd
yuriyfomin17 <fominyura2012@gmail.com>
2020-12-23 15:09:49 +0200

    updated

--- LeetCode/Binary Search Tree/Basic Operations in BST/Delete the node in BST.js
+++ LeetCode/Binary Search Tree/Basic Operations in BST/Delete the node in BST.js
@@ -11,59 +11,52 @@
  * @param {number} key
  * @return {TreeNode}
  */
+// Time complexity is O(logN). During execution we go down the tree all the time.
+    // First search the node to delete and then actually delete it. Search takes H1 from
+    // root to the node to delete. And H2is the tree heaigh from the root to delete to the leaf
+    // O(H1 + H2) = O(H). H is the tree height which is equal to the logN where N is the number of nodes in
+    // balanced tree.
+
+    // Space Complexity is O(H) to keep the recursion stack where H = log N for the balanced tree
 const deleteNode = function (root, key) {
     if (!root) {
         return null
     }
-    let currNode = root
-    let previous = null
     const successor = (node) => {
-        let previous = node
+        // One Step right and then always left
         node = node.right
         while (node.left) {
-            previous = node
             node = node.left
         }
-        const value = node.val
-        previous.right = null
-        return value
+        return node.val
     }
-    while (currNode) {
-        if (currNode.val < key) {
-            previous = currNode
-            currNode = currNode.right
-        } else if (currNode.val > key) {
-            previous = currNode
-            currNode = currNode.left
-        } else if (currNode.val === key) {
-            if (currNode.left === null && currNode.right === null) {
-                if(previous===null){
-                    return null
-                }
-                if (previous.right === currNode) {
-                    previous.right = null
-                } else if (previous.left === currNode) {
-                    previous.left = null
-                }
-            } else if (currNode.left !== null && currNode.right !== null) {
-                const replacement = successor(currNode)
-                currNode.val = replacement
-                return root
-            } else if (currNode.left === null && currNode.right !== null) {
-                if (previous.right === currNode) {
-                    previous.right = currNode.right
-                } else if (previous.left === currNode) {
-                    previous.left = currNode.right
-                }
-                return root
-            } else if (currNode.left !== null && currNode.right === null) {
-                if (previous.right === currNode) {
-                    previous.right = currNode.left
-                } else if (previous.left === currNode) {
-                    previous.left = currNode.left
-                }
-                return root
-            }
+    const predecessor = (node) => {
+        // One step left and then always right
+        node = node.left
+        while (node.right) {
+            node = node.right
+        }
+        return node.val
+    }
+    // delete from the right subtree
+    if (key > root.val) {
+        root.right = deleteNode(root.right, key)
+    // delete from the left subtree
+    } else if (key < root.val) {
+        root.left = deleteNode(root.left, key)
+    // delete current node
+    } else {
+        // node is leaf
+        if (!root.left && !root.right) {
+            root = null
+            // node is not leaf and has right child
+        } else if (root.right) {
+            root.val = successor(root)
+            root.right = deleteNode(root.right, root.val)
+        }else {
+            // node is not leaf and has left child
+            root.val = predecessor(root)
+            root.left = deleteNode(root.left, root.val)
         }
     }
     return root
