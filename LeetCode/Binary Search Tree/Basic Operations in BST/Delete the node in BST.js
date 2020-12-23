398f79f227c5c55d10615d86ea8875062018efee
yuriyfomin17 <fominyura2012@gmail.com>
2020-12-23 16:19:11 +0200

    updated

--- LeetCode/Binary Search Tree/Basic Operations in BST/Delete the node in BST.js
+++ LeetCode/Binary Search Tree/Basic Operations in BST/Delete the node in BST.js
@@ -11,6 +11,7 @@
  * @param {number} key
  * @return {TreeNode}
  */
+//RECURSIVE SOLUTION
 // Time complexity is O(logN). During execution we go down the tree all the time.
     // First search the node to delete and then actually delete it. Search takes H1 from
     // root to the node to delete. And H2is the tree heaigh from the root to delete to the leaf
@@ -62,3 +63,73 @@ const deleteNode = function (root, key) {
         return root
     };
 
+
+
+
+// ITERATIVE SOLUTION
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} key
+ * @return {TreeNode}
+ */
+// Time Complexity in best case is Olog(N) where N is the number of nodes in best case but O(N) in worst case
+// Space Complexity is O(1) since we don't use any extra memeory to keep tree
+const deleteNode = function (root, key) {
+    if (!root) {
+        return null
+    }
+    const successor = (node) => {
+        node = node.right
+        while (node.left) {
+            node = node.left
+        }
+        return node.val
+    }
+    const predecessor = (node) => {
+        node = node.left
+        while (node.right) {
+            node = node.right
+        }
+        return node.val
+    }
+
+    let currNode = root
+    let previous = null
+    while (currNode) {
+        if (currNode.val > key) {
+            previous = currNode
+            currNode = currNode.left
+        } else if (currNode.val < key) {
+            previous = currNode
+            currNode = currNode.right
+        } else if (currNode.val === key) {
+            if(previous===null && currNode.left === null && currNode.right === null){
+                return null
+            }
+            if (previous!==null && currNode.left === null && currNode.right === null) {
+                previous.left === currNode ? previous.left = null : previous.right = null
+                return root
+            }
+            if(currNode.right){
+                currNode.val = successor(currNode)
+                previous = currNode
+                key = currNode.val
+                currNode = currNode.right
+            }else {
+                currNode.val = predecessor(currNode)
+                previous = currNode
+                key = currNode.val
+                currNode = currNode.left
+            }
+        }
+    }
+    return root
+};
--- LeetCode/train.js
+++ LeetCode/train.js
@@ -1,25 +1,66 @@
 /**
- * @param {string} s
- * @return {number}
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
  */
-const lengthOfLongestSubstring = function (s) {
-    if (s.length === 0) {
-        return 0
+/**
+ * @param {TreeNode} root
+ * @param {number} key
+ * @return {TreeNode}
+ */
+// Time Complexity in best case is Olog(N) where N is the number of nodes in best case but O(N) in worst case
+// Space Complexity is O(1) since we don't use any extra memeory to keep tree
+const deleteNode = function (root, key) {
+    if (!root) {
+        return null
     }
-    const dict = {}
-    let start = 0
-    let finish = 0
-    let maxLength = 0
-    while (start < s.length && finish < s.length){
-        if (!dict[s.charAt(finish)]) {
-            dict[s.charAt(finish)] = 1
-            finish += 1
-            maxLength = Math.max(maxLength, finish - start)
-        } else {
-            delete dict[s.charAt(start)]
-            start += 1
+    const successor = (node) => {
+        node = node.right
+        while (node.left) {
+            node = node.left
+        }
+        return node.val
+    }
+    const predecessor = (node) => {
+        node = node.left
+        while (node.right) {
+            node = node.right
+        }
+        return node.val
+    }
+
+    let currNode = root
+    let previous = null
+    while (currNode) {
+        if (currNode.val > key) {
+            previous = currNode
+            currNode = currNode.left
+        } else if (currNode.val < key) {
+            previous = currNode
+            currNode = currNode.right
+        } else if (currNode.val === key) {
+            if(previous===null && currNode.left === null && currNode.right === null){
+                return null
+            }
+            if (previous!==null && currNode.left === null && currNode.right === null) {
+                previous.left === currNode ? previous.left = null : previous.right = null
+                return root
+            }
+            if(currNode.right){
+                currNode.val = successor(currNode)
+                previous = currNode
+                key = currNode.val
+                currNode = currNode.right
+            }else {
+                currNode.val = predecessor(currNode)
+                previous = currNode
+                key = currNode.val
+                currNode = currNode.left
+            }
         }
     }
-    return maxLength
-
+    return root
 };
