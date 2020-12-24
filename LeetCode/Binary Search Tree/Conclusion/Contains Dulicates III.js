b56d80e05a88e385344c6edf1f259b2b74654439
yuriyfomin17 <fominyura2012@gmail.com>
2020-12-24 13:03:20 +0200

    updated

--- LeetCode/Binary Search Tree/Conclusion/Contains Dulicates III.js
+++ LeetCode/Binary Search Tree/Conclusion/Contains Dulicates III.js
@@ -8,12 +8,22 @@
     // Note that we do at most n comparisons in one search even if k can be larger than n
     // Space complexity is O(1)
 const containsNearbyAlmostDuplicate = function (nums, k, t) {
-    for (let i = 0; i < nums.length; i++) {
-        for (let j = Math.max(0, i - k); j < i; j++) {
-            if (Math.abs(nums[i] - nums[j]) <= t) {
-                return true
+        for (let i = 0; i < nums.length; i++) {
+            for (let j = Math.max(0, i - k); j < i; j++) {
+                if (Math.abs(nums[i] - nums[j]) <= t) {
+                    return true
+                }
             }
         }
-    }
-    return false
+        return false
+    };
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @param {number} t
+ * @return {boolean}
+ */
+const containsNearbyAlmostDuplicate = function (nums, k, t) {
+
 };
