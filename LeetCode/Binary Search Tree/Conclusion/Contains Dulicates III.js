d86359eb6196ae1a64b0703e4afc21cae4521c34
yuriyfomin17 <fominyura2012@gmail.com>
2020-12-24 14:44:12 +0200

    updated

--- LeetCode/Binary Search Tree/Conclusion/Contains Dulicates III.js
+++ LeetCode/Binary Search Tree/Conclusion/Contains Dulicates III.js
@@ -18,12 +18,20 @@ const containsNearbyAlmostDuplicate = function (nums, k, t) {
         return false
     };
 
-/**
- * @param {number[]} nums
- * @param {number} k
- * @param {number} t
- * @return {boolean}
- */
-const containsNearbyAlmostDuplicate = function (nums, k, t) {
-
+const containsNearbyAlmostDuplicateB = function (nums, k, t) {
+    let bucket = {}, w = t + 1;
+    debugger
+    for (let i = 0; i < nums.length; i++) {
+        let idx = Math.floor(nums[i] / w);
+        if (bucket.hasOwnProperty(idx)) return true;
+        else if (bucket.hasOwnProperty(idx + 1) && Math.abs(nums[i] - bucket[idx + 1]) < w) return true;
+        else if (bucket.hasOwnProperty(idx - 1) && Math.abs(nums[i] - bucket[idx - 1]) < w) return true;
+        bucket[idx] = nums[i];
+        if (i >= k) {
+            delete bucket[Math.floor(nums[i - k] / w)];
+        }
+    }
+    return false;
 };
+
+console.log(containsNearbyAlmostDuplicateB([1, 2, 3, 1], 3, 0))
