75eafcac51ca1807f2883d1ff0678d8585e6e31e
yuriyfomin17 <fominyura2012@gmail.com>
2020-12-24 15:08:02 +0200

    updated

--- LeetCode/Binary Search Tree/Conclusion/Contains Dulicates III.js
+++ LeetCode/Binary Search Tree/Conclusion/Contains Dulicates III.js
@@ -18,20 +18,34 @@ const containsNearbyAlmostDuplicate = function (nums, k, t) {
         return false
     };
 
-const containsNearbyAlmostDuplicateB = function (nums, k, t) {
-    let bucket = {}, w = t + 1;
-    debugger
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @param {number} t
+ * @return {boolean}
+ */
+// Time Complexity is O(N) for each of the n elements we do at most three searches
+    // one insert and one delete on the HashMap which costs
+    // constant time on average. Thus entire algorithm costs O(N) time
+
+    // Space Complexity is O(min(n, k)). Space is dominated by the hashMap which is
+    // linear to the size of its elements. The size of HasMap is upper bounded by both n and k
+const containsNearbyAlmostDuplicate = function (nums, k, t) {
+    const buckets = {}
+    const range = t + 1
     for (let i = 0; i < nums.length; i++) {
-        let idx = Math.floor(nums[i] / w);
-        if (bucket.hasOwnProperty(idx)) return true;
-        else if (bucket.hasOwnProperty(idx + 1) && Math.abs(nums[i] - bucket[idx + 1]) < w) return true;
-        else if (bucket.hasOwnProperty(idx - 1) && Math.abs(nums[i] - bucket[idx - 1]) < w) return true;
-        bucket[idx] = nums[i];
+        let bucketIndex = Math.floor(nums[i] / range)
+        if (buckets.hasOwnProperty(bucketIndex)) {
+            return true
+        } else if (buckets.hasOwnProperty(bucketIndex + 1) && Math.abs(nums[i] - buckets[bucketIndex + 1]) < range) {
+            return true
+        } else if (buckets.hasOwnProperty(bucketIndex - 1) && Math.abs(nums[i] - buckets[bucketIndex - 1]) < range) {
+            return true
+        }
+        buckets[bucketIndex] = nums[i]
         if (i >= k) {
-            delete bucket[Math.floor(nums[i - k] / w)];
+            delete buckets[Math.floor(nums[i - k] / range)]
         }
     }
-    return false;
+    return false
 };
-
-console.log(containsNearbyAlmostDuplicateB([1, 2, 3, 1], 3, 0))
