2a8973f698df6c4b4231312dff01d855fe99f2f2
yuriyfomin17 <fominyura2012@gmail.com>
2020-12-25 12:32:06 +0200

    updated

---
+++ LeetCode/Goldman Sachs/Reaching Points.js
@@ -0,0 +1,43 @@
+/**
+ * @param {number} sx
+ * @param {number} sy
+ * @param {number} tx
+ * @param {number} ty
+ * @return {boolean}
+ */
+const reachingPoints = function (sx, sy, tx, ty) {
+    while (tx >= sx && ty >= sy) {
+        if (sx === tx && sy === ty) {
+            return true
+        }
+        if (tx > ty) {
+            tx -= ty
+        } else {
+            ty -= tx
+        }
+    }
+    return false
+};
+
+const reachingPoints2 = function (sx, sy, tx, ty) {
+    while (tx >= sx && ty >= sy) {
+        if (tx === ty) {
+            break
+        }
+        if (tx > ty) {
+            if (ty > sy) {
+                tx %= ty
+            } else {
+                return (tx - sx) % ty === 0
+            }
+        } else {
+            if (tx > sx) {
+                ty %= tx
+            } else {
+                return (ty - sy) % tx === 0
+            }
+        }
+
+    }
+    return (tx === sx && ty === sy)
+};
