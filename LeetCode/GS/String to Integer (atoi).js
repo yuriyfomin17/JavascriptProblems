fed1725a933ea3c8acf01e8a42299d5bd4866970
yuriyfomin17 <fominyura2012@gmail.com>
2020-12-28 13:23:13 +0200

    updated

--- LeetCode/GS/String to Integer (atoi).js
+++ LeetCode/GS/String to Integer (atoi).js
@@ -9,3 +9,65 @@ const myAtoi = function(str) {
 console.log(
     myAtoi("   +0 123")
 )
+
+const MIN = -2147483648
+const MAX = 2147483647
+const NUMBERS = '0123456789'
+
+/**
+ * @param {string} str
+ * @return {number}
+ */
+var myAtoi = function(str) {
+    let answer = 0
+
+    let isOnNumber = false
+    let sign = null
+
+    for (const char of str) {
+        if (!isOnNumber) {
+            if (char === ' ') {
+                if (sign) return 0
+                continue
+            }
+
+            if (char === '+' || char === '-') {
+                if (sign) return 0
+                sign = char
+                continue
+            }
+
+            const isNumber = NUMBERS.includes(char)
+
+            if (!isNumber) {
+                return 0
+            }
+
+            isOnNumber = true
+            if (!sign) sign = '+'
+        }
+
+        if (isOnNumber) {
+            const num = Number(char)
+            if (char === ' ') break
+            if (Number.isNaN(num)) break
+
+            if (sign === '-') {
+                if (-1 * answer - num / 10 < MIN / 10) return MIN
+            } else {
+                if (answer + num / 10 > MAX / 10) return MAX
+            }
+
+            answer = answer * 10 + num
+        }
+    }
+
+    if (answer === 0) return 0
+
+    const multipliers = {
+        '+': 1,
+        '-': -1
+    }
+
+    return answer * multipliers[sign]
+};
