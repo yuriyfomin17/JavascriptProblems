3c8f8e30007ebbcef024d97ab49fc080050ac87e
yuriyfomin17 <fominyura2012@gmail.com>
2021-01-21 17:52:17 +0200

    Problem Solved

--- LeetCode/Apple/Arrays and Strings/First Unique Character in a String.js
+++ LeetCode/Apple/Arrays and Strings/First Unique Character in a String.js
@@ -2,28 +2,34 @@
  * @param {string} s
  * @return {number}
  */
-// Time Complexity is O(N)
-    // Space Complexity is O(N)
+//Time complexity is O(N^2) since we iterate over array each time for each char hence N*N
+// Space complexity : no extra space is taken O(1)
 const firstUniqChar = function (s) {
-    const dict = {}
-    for (let i = 0; i < s.length; i++) {
-        if (dict[s[i]] === undefined) {
-            dict[s[i]] = [i, 1]
-        } else {
-            dict[s[i]][1] = dict[s[i]][1] + 1
+    const size = s.length
+    for (let i = 0; i < size; i++) {
+        if (s.indexOf(s.charAt(i)) === s.lastIndexOf(s.charAt(i))) {
+            return i
         }
     }
-    for (let value of Object.values(dict)) {
+    return -1
+};
+// Theoretically HashMap is better here which takes O(N) since we iterate only once. However, tope approach is better
+// Space complexity is O(K) where K is the size of and ditconary which at most can be equal to the size of the string
+const firstUniqCharH = function (s) {
+    debugger
+    const size = s.length
+    const dict = {}
+    for (let i = 0; i < size; i++) {
+        if (!dict[s.charAt(i)]) {
+            dict[s.charAt(i)] = [i,1]
+        } else {
+            dict[s.charAt(i)][1] = dict[s.charAt(i)][1] + 1
+        }
+    }
+    for (let [key, value] of Object.entries(dict)) {
         if (value[1] === 1) {
             return value[0]
         }
     }
     return -1
-
 };
-
-console.log(
-    firstUniqChar(
-        "loveleetcode"
-    )
-)
