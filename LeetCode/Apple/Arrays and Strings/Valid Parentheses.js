19aa4e96ab349322c6916981c39c9c6530df425d
yuriyfomin17 <fominyura2012@gmail.com>
2021-01-21 18:51:21 +0200

    Problem Solved

---
+++ LeetCode/Apple/Arrays and Strings/Valid Parentheses.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+// Time Complexity is O(N) where N is the number of chars in array
+// Space Complexity is O(N) where N is the number of elements stored in stack
+const isValid = function (s) {
+    const arr = s.split('')
+    const stack = []
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] !== '}' && arr[i] !== ']' && arr[i] !== ')') {
+            stack.push(arr[i])
+        } else {
+            if (stack[stack.length - 1] === '{' && arr[i] === '}') {
+                stack.pop()
+            } else if (stack[stack.length - 1] === '(' && arr[i] === ')') {
+                stack.pop()
+            } else if (stack[stack.length - 1] === '[' && arr[i] === ']') {
+                stack.pop()
+            } else {
+                return false
+            }
+        }
+    }
+    return stack.length === 0
+};
