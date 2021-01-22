03fcd6bb4eeeaa64f0a4422e23bdf1ee2046b797
yuriyfomin17 <fominyura2012@gmail.com>
2021-01-22 16:39:17 +0200

    Problem Solved

--- LeetCode/Apple/Recursion/Combination Sum.js
+++ LeetCode/Apple/Recursion/Combination Sum.js
@@ -4,5 +4,24 @@
  * @return {number[][]}
  */
 const combinationSum = function (candidates, target) {
-
+    const output = []
+    const helper = (remainder, path, start) => {
+        if (remainder < 0) {
+            return
+        }
+        if (remainder === 0) {
+            output.push([...path])
+        }
+        for (let i = start; i < candidates.length; i++) {
+            helper(remainder - candidates[i], [...path, candidates[i]], i)
+        }
+    }
+    helper(target,[], 0)
+    return output
 };
+
+console.log(
+    combinationSum(
+        [2, 3, 6, 7], 7
+    )
+)
