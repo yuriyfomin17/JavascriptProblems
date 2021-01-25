4cd1a3c065eca6dd65c8a7ef59f40bdfa8b7e2ad
yuriyfomin17 <fominyura2012@gmail.com>
2021-01-25 14:10:46 +0200

    Problem Solved

---
+++ LeetCode/Apple/Sorting and Searching/Merge Intervals.js
@@ -0,0 +1,25 @@
+// Time Complexity is O(Nlog(N)) + O(N) for iterating
+// Space Complexity is O(1)
+const merge = function (intervals) {
+    if (intervals.length === 0) {
+        return []
+    }
+    intervals.sort(function (a, b) {
+        if (a[0] >= b[0]) {
+            return 1
+        } else {
+            return -1
+        }
+    })
+    let pointer = 1
+    while (pointer < intervals.length) {
+        if (intervals[pointer - 1][1] >= intervals[pointer][0]) {
+            let end = Math.max(intervals[pointer][1], intervals[pointer - 1][1])
+            intervals[pointer - 1][1] = end
+            intervals.splice(pointer, 1)
+        }else {
+            pointer += 1
+        }
+    }
+    return intervals
+};
